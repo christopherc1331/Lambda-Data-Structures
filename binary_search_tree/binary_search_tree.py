5e3a10720ae444f6abc0b0b75e9e8c7a895a3174
Chris Cruz <christopherc1331@gmail.com>
2020-05-28 23:43:14 -0700

    Finished all binary tree exercises

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,7 @@
 """
-Binary search trees are a data structure that enforce an ordering over 
-the data they store. That ordering in turn makes it a lot more efficient 
-at searching for a particular piece of data in the tree. 
+Binary search trees are a data structure that enforce an ordering over
+the data they store. That ordering in turn makes it a lot more efficient
+at searching for a particular piece of data in the tree.
 
 This part of the project comprises two days:
 1. Implement the methods `insert`, `contains`, `get_max`, and `for_each`
@@ -9,6 +9,9 @@ This part of the project comprises two days:
 2. Implement the `in_order_print`, `bft_print`, and `dft_print` methods
    on the BSTNode class.
 """
+
+from collections import deque
+from stack import Stack
 
 
 class BSTNode:
@@ -19,20 +22,17 @@ class BSTNode:
 
     # Insert the given value into the tree
     def insert(self, value):
-        new_node = BSTNode(value)
-        if self.value:
-            if self.value > value:
-                if self.left:
-                    self.left.insert(value)
-                else:
-                    self.left = new_node
+
+        if self.value > value:
+            if self.left:
+                self.left.insert(value)
             else:
-                if self.right:
-                    self.right.insert(value)
-                else:
-                    self.right = new_node
+                self.left = BSTNode(value)
         else:
-            self.value = new_node
+            if self.right:
+                self.right.insert(value)
+            else:
+                self.right = BSTNode(value)
 
     # Return True if the tree contains the value
     # False if it does not
@@ -40,28 +40,25 @@ class BSTNode:
     def contains(self, target):
         """ check if current value exists then check if current value is equal to target
         if the target is less than the current value then check to see if there is a left value
-        if there isn't then return false if there is then you pass the left value into the 
-        contains function vice versa for right. at the end you handle the else statement by 
+        if there isn't then return false if there is then you pass the left value into the
+        contains function vice versa for right. at the end you handle the else statement by
         returning false"""
 
-        if self.value:
-            if self.value == target:
-                return True
+        if self.value == target:
+            return True
+
+        if self.value > target:
+            if self.left:
+                return self.left.contains(target)
             else:
-                if self.value > target:
-                    if self.left:
-                        return self.left.contains(target)
-                    else:
-                        return False
-                else:
-                    if self.right:
-                        # return self.right.contains(target)
-                        eval = self.right.contains(target)
-                        return eval
-                    else:
-                        return False
+                return False
         else:
-            return False
+            if self.right:
+                return self.right.contains(target)
+                # eval = self.right.contains(target)
+                # return eval
+            else:
+                return False
 
     # Return the maximum value found in the tree
     def get_max(self):
@@ -71,11 +68,22 @@ class BSTNode:
         # return self.right.get_max ()
         # if there isn't a right value then return self.value
 
-        if self.value:
-            if self.right:
-                return self.right.get_max()
-            else:
-                return self.value
+        if self.right:
+            return self.right.get_max()
+        else:
+            return self.value
+
+    def get_min(self):
+
+        # check if there's a value
+        # check if there's a left.value
+        # return self.left.get_max ()
+        # if there isn't a left value then return self.value
+
+        if self.left:
+            return self.right.get_min()
+        else:
+            return self.value
 
     # Call the function `fn` on the value of each node
 
@@ -91,22 +99,48 @@ class BSTNode:
     # Print all the values in order from low to high
     # Hint:  Use a recursive, depth first traversal
     def in_order_print(self, node):
-        pass
+        if node.left is not None:
+            node.left.in_order_print(node.left)
 
+        print(self.value)
+
+        if self.right is not None:
+            node.right.in_order_print(node.right)
     # Print the value of every node, starting with the given node,
     # in an iterative breadth first traversal
+
     def bft_print(self, node):
-        pass
+        queue = deque()
+        queue.append(self)
+
+        while len(queue) > 0:
+            current = queue.popleft()
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            print(current.value)
 
     # Print the value of every node, starting with the given node,
     # in an iterative depth first traversal
+
     def dft_print(self, node):
-        pass
+        stack = Stack()
+        stack.push(node)
+
+        while len(stack) > 0:
+            current = stack.pop()
+            if current.left:
+                stack.push(current.left)
+            if current.right:
+                stack.push(current.right)
+            print(current.value)
 
     # Stretch Goals -------------------------
     # Note: Research may be required
 
     # Print Pre-order recursive DFT
+
     def pre_order_dft(self, node):
         pass
 
